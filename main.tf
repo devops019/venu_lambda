69b66034516f463ab454fbf84f89df7a259475d2
venu <venugopal.k1994@gmail.com>
2021-09-10 11:02:47 -0400

    prac

--- main.tf
+++ main.tf
@@ -18,7 +18,7 @@ resource "aws_iam_role" "iam_for_lambda" {
 EOF
 }
 
-resource "aws_lambda_function" "test_lambda" {
+resource "aws_lambda_function" "venu_lambda" {
   filename      = "lambda_function_payload.zip"
   function_name = "lambda_function_name"
   role          = aws_iam_role.iam_for_lambda.arn
